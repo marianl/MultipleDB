795a5d37e275d530ab61cc2a6ffe4de76089df07
maria <maria@dummy.org.es>
2013-05-17 17:53:08 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MultipleDB::Application.config.secret_key_base = 'f9e8bc1e408b4fc8b09cefa71947c476372558d05e016a2f369c4078ce9428228d11556fc1c115a6f83c3677c24e9cccb0dabf393493eab9d292ff033d9881d1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MultipleDB::Application.config.session_store :cookie_store, key: '_MultipleDB_session'
